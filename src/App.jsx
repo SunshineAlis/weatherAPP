6b2ceefdae8ec3b98f9fa76b02fd2dcc3859e41a
SunshineAlis <bumaam@yahoo.com>
2025-01-21 19:39:32 +0800

    1

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,13 @@ import DaySnow from "./assets/icons/DaySnow.webp";
 import DayStorm from "./assets/icons/DayStorm.webp";
 import DayWind from "./assets/icons/DayWind.webp";
 import DayClouds from "./assets/icons/DayClouds.webp";
-// import NightRain from './assets/icons/NightRain.webp';
-//import Sunny from "./assets/icons/Sunny.webp"
+import NightRain from './assets/icons/NightRain.webp';
+import DaySun from "./assets/icons/DaySun.webp"
 import NightSnow from "./assets/icons/NightSnow.webp";
 import NightStorm from "./assets/icons/NightStorm.webp";
 import NightWind from "./assets/icons/NightWind.webp";
 import NightClouds from "./assets/icons/NightClouds.webp";
-
-
+import NightMoon from "./assets/icons/NightMoon.webp";
 
 function WeatherApp() {
   const inputRef = useRef();
@@ -43,6 +42,24 @@ function WeatherApp() {
   });
 
   const weatherApiKey = "43c89d3aae3d479da26122821251801";
+  const getAirQuality = (index) => {
+    switch (index) {
+      case 1:
+        return "Good";
+      case 2:
+        return "Moderate";
+      case 3:
+        return "Unhealthy for Sensitive Groups";
+      case 4:
+        return "Unhealthy ";
+      case 5:
+        return "Very Unhealthy";
+      case 6:
+        return "Hazardous";
+      default:
+        return "N/A";
+    }
+  };
 
   const fetchCitySuggestions = async (query) => {
     if (!query.trim()) return;
@@ -63,7 +80,7 @@ function WeatherApp() {
 
     try {
       setLoading(true);
-      const url = `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${city}`;
+      const url = `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${city}&aqi=yes`;
       const response = await fetch(url);
       const data = await response.json();
       setWeatherData(data);
@@ -93,6 +110,10 @@ function WeatherApp() {
     if (conditionText.includes("rain") || conditionText.includes("drizzle") || conditionText.includes("fog") || conditionText.includes("overcast") || conditionText.includes("mist")) {
       return <img src={DayRain} alt="Day Rain Icon" />;
     }
+
+    if (conditionText.includes("sunny")  ) {
+      return <img src={DaySun} alt="Day Sun Icon" />;
+    }
     if (conditionText.includes("snow") || conditionText.includes("ice") || conditionText.includes("blizzard") || conditionText.includes("sleet")) {
       return <img src={DaySnow} alt="Day Snow Icon" />;
     }
@@ -103,7 +124,7 @@ function WeatherApp() {
       return <img src={DayWind} alt="Day Wind Icon" />;
     }
     if (conditionText.includes("cloud")) {
-      return <img src={DayClouds} alt="Day Clouds Icon" />;
+      return <img src={DaySun} alt="Day Sun Icon" />;
     }
 
     return <img src={DayClouds} alt="Day Clouds Icon" />;
@@ -112,28 +133,37 @@ function WeatherApp() {
   const getNightIcon = () => {
     if (!weatherData?.current?.condition?.text) return null;
     const conditionText = weatherData.current.condition.text.toLowerCase();
-
+    if (conditionText.includes("rain") || conditionText.includes("drizzle") || conditionText.includes("fog") || conditionText.includes("overcast") || conditionText.includes("mist")) {
+      return <img src={NightRain} alt="Night Rain Icon" />;
+    }
     if (conditionText.includes("snow") || conditionText.includes("ice") || conditionText.includes("blizzard") || conditionText.includes("sleet")) {
       return <img src={NightSnow} alt="Night Snow Icon" />;
     }
     if (conditionText.includes("thunder") || conditionText.includes("storm")) {
       return <img src={NightStorm} alt="Night Storm Icon" />;
     }
-    if (conditionText.includes("wind")) {
-      return <img src={NightWind} alt="Night Wind Icon" />;
-    }
     if (conditionText.includes("cloud")) {
       return <img src={NightClouds} alt="Night Clouds Icon" />;
     }
-    return <img src={NightClouds} alt="Night Clouds Icon" />;
+    
+    if (conditionText.includes("wind")) {
+      return <img src={NightWind} alt="Night Wind Icon" />;
+    }
+    
+    if (conditionText.includes("clear")) {
+      return <img src={NightMoon} alt="Night Moon Icon" />;
+    }
+    return <img src={NightMoon} alt="Night Moon Icon" />;
+
   };
+
 
   useEffect(() => {
     search(selectedCity);
   }, []);
   
   return (
-    <div className="bg-slate-150 w-[100%] h-[850px] m-auto mt-[100px] relative">
+    <div className="bg-slate-150 w-[100%] h-[850px] m-auto mt-[50px] relative">
       {/* Design circles */}
       <div className="absolute top-[395px] left-[490px] w-[250px] h-[250px] border border-slate-200 rounded-full"></div>
       <div className="absolute top-[300px] left-[400px] w-[470px] h-[450px] border border-slate-200 rounded-full"></div>
@@ -203,14 +233,18 @@ function WeatherApp() {
         <div className="w-[170px] h-[70px] absolute top-[120px] left-[70px]">
         {getDayIcon()}
         </div>
-        <span className="font-semibold text-[55px] text-gray-800 absolute top-[265px] left-[15px]">
+        <span className="font-semibold text-[50px] text-gray-800 absolute top-[265px] left-[15px]">
           {weatherData?.current?.temp_c || "N/A"}°C
         </span>
         <p className=" text-[20px] text-gray-900 absolute top-[329px] left-[20px]">
         {weatherData?.forecast?.forecastday?.[0]?.day?.condition?.text || "N/A"}
-         
         </p>
-        <span className="absolute top-[355px] left-[20px]  text-[20px] text-gray-900"> Air quality</span>
+              <ul className="absolute top-[355px] left-[20px]  text-[18px] text-gray-900">
+                <li>
+                  Air quality:{" "}
+                  {getAirQuality(weatherData?.current?.air_quality?.["us-epa-index"])}
+                </li>
+              </ul>
         <div className="font-semibold text-gray-800 flex items-center gap-[21px] absolute top-[87%] left-[7%]">
           <Hom />
           <Loct />
@@ -237,7 +271,7 @@ function WeatherApp() {
         <div className="flex items-center justify-center w-[140px] h-[50px] absolute top-[180px] left-[70px]">
         {getNightIcon()}
         </div>
-        <span className="font-semibold text-[55px] text-gray-100 absolute top-[260px] left-[20px]">
+        <span className="font-semibold text-[50px] text-gray-100 absolute top-[260px] left-[20px]">
           {weatherData?.forecast?.forecastday?.[0]?.day?.mintemp_c || "N/A"}°C
         </span>
         <p className="text-[20px] text-gray-200 absolute top-[340px] left-[20px]">
